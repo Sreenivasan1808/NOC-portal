d47e9bcc502c2b7136395feac3048ad23d3ea90a
Sreeksha <sreekshasree270@gmail.com>
2025-11-02 22:35:50 +0530

    change-password

--- backend/src/middlewares/jwtAuth.ts
+++ backend/src/middlewares/jwtAuth.ts
@@ -16,3 +16,15 @@ export function authenticateJWT(req: Request, res: Response, next: NextFunction)
         return res.status(401).json({ message: 'Invalid or expired token.' });
     }
 }
+export const protect = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Not authorized, no token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded; // { id: userId, ... }
+    next();
+  } catch {
+    return res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { forgotPassword, verifyOtp, resetPassword, handleLogin } from '../controllers/authController.js';
+import { forgotPassword,changePassword, verifyOtp, resetPassword, handleLogin } from '../controllers/authController.js';
 
 const router = express.Router();
 
@@ -7,7 +7,6 @@ router.post('/forgot-password', forgotPassword);
 router.post('/verify-otp', verifyOtp);
 router.post('/reset-password', resetPassword);
 router.post('/login', handleLogin);
-
+router.post("/change-password", protect, changePassword);
 router.get("/", (req, res) => res.send("Auth route"))
-
 export default router;
